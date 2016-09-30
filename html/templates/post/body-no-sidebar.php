16bd6777209061ca93b9b012b6aa3d8c05913b80
Sem Schilder <me@sem.design>
2016-09-30 14:53:33 +0200

    Blog changes

--- html/templates/post/body-no-sidebar.php
+++ html/templates/post/body-no-sidebar.php
@@ -2,14 +2,14 @@
 			<main class="main-page">
 				<?php if (has_post_thumbnail( get_the_ID() ) ): ?>
 					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'single-post-thumbnail' ); ?>
-					<div class="vastgoed--featured-image" style="background-image: url('<?php echo $image[0]; ?>')">
+					<div class="post--featured-image" style="background-image: url('<?php echo $image[0]; ?>')">
 					</div>
 					<?php endif; ?>
 				<?php the_content(); ?>
-				<ul class="vastgoed--description__list">
+				<ul class="post--description__list">
 					<?php foreach (get_field('extra_informatie') as $info){
 						?>
-					<li> <span class="vastgoed--description__item"><?php echo $info['titel'] ?>:</span> <?php echo $info['info'] ?></li>
+					<li> <span class="post--description__item"><?php echo $info['titel'] ?>:</span> <?php echo $info['info'] ?></li>
 						<?php
 					}?>
 				</ul>
--- html/templates/post/body-sidebar.php
+++ html/templates/post/body-sidebar.php
@@ -2,7 +2,7 @@
 			<main class="main-page__sidebar">
 				<?php if (has_post_thumbnail( get_the_ID() ) ): ?>
 					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'single-post-thumbnail' ); ?>
-					<div class="vastgoed--featured-image" style="background-image: url('<?php echo $image[0]; ?>')">
+					<div class="post--featured-image" style="background-image: url('<?php echo $image[0]; ?>')">
 					</div>
 					<?php endif; ?>
 				<?php get_template_part('gallery') ?>
